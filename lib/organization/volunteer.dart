7a45f453929aa887f9ceb8ac4982b00e8b25a91d
Alex-George1 <kulampallil.alex@gmail.com>
2025-04-02 16:01:57 +0530

    organization/volunteer

--- lib/organization/volunteer.dart
+++ lib/organization/volunteer.dart
@@ -1,29 +1,65 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import '../../../styles/styles.dart';
 
-class VolunteerDetailsPage extends StatelessWidget {
-  const VolunteerDetailsPage({super.key});
+class VolunteerDetailsPage extends StatefulWidget {
+  final String volunteerId; // Unique identifier for the volunteer
+  const VolunteerDetailsPage({super.key, required this.volunteerId});
+
+  @override
+  State<VolunteerDetailsPage> createState() => _VolunteerDetailsPageState();
+}
+
+class _VolunteerDetailsPageState extends State<VolunteerDetailsPage> {
+  // Volunteer details stored as a JSON-like Map
+  bool isLoading = true; // Loading state for the page
+  Map<String, dynamic> volunteerDetails={};
+  String neighbourhoodName="";
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    details(); // Fetch volunteer details when the page is initialized
+
+  }
+   Future<void> details() async {
+    try {
+      print(widget.volunteerId);
+      DocumentSnapshot<Map<String, dynamic>> querySnapshot = await FirebaseFirestore.instance
+          .collection('volunteers')
+          .doc(widget.volunteerId)
+          .get();
+      volunteerDetails = querySnapshot.data() ?? {};
+      print(volunteerDetails);
+      DocumentSnapshot<Map<String, dynamic>> querySnapshot2 = await FirebaseFirestore.instance
+      .collection('neighbourhood')
+      .doc(volunteerDetails["neighbourhoodId"])
+      .get();
+  print(volunteerDetails["neighbourhoodId"]);
+  print(querySnapshot2.data()?["name"]);
+  neighbourhoodName = querySnapshot2.data()?["name"]??"";
+       setState(() {
+      isLoading = false; // Update loading state
+      // Update loading state
+    });
+    } catch (e) {
+      debugPrint("Error fetching Firestore data: $e");
+    } // Simulate network delay
+   }
+
+   
 
   @override
   Widget build(BuildContext context) {
-    // Volunteer details stored as a JSON-like Map
-    final Map<String, dynamic> volunteerDetails = {
-      "name": "Blesson K Tomy",
-      "age": "22",
-      "gender": "Male",
-      "rating": "4.8",
-      "neighbourhood": "Pala",
-      "requestsCompleted": "32",
-      "servicesProvided": [
-        "Grocery Pickup",
-        "Good Morning",
-        "Gardening",
-        "Medicine Delivery",
-        "Driving",
-        "IEEE President"
-        ],
-    };
-
+    if (isLoading) {
+       return const Scaffold(
+        backgroundColor: Styles.darkPurple,
+        body: Center(
+          child: CircularProgressIndicator(valueColor: AlwaysStoppedAnimation<Color>(Colors.white),),
+        ),
+      );
+    }
+    else{
     return Scaffold(
       backgroundColor: Styles.darkPurple, // Set background color
       body: SingleChildScrollView(
@@ -36,7 +72,11 @@ class VolunteerDetailsPage extends StatelessWidget {
                 children: [
                   const Align(
                     alignment: Alignment.center,
-                    child: Text("Volunteer Details", style: Styles.titleStyle, textAlign: TextAlign.center,),
+                    child: Text(
+                      "Volunteer Details",
+                      style: Styles.titleStyle,
+                      textAlign: TextAlign.center,
+                    ),
                   ),
                   Positioned(
                     bottom: 20,
@@ -49,7 +89,6 @@ class VolunteerDetailsPage extends StatelessWidget {
                 ],
               ),
             ),
-
             Padding(
               padding: const EdgeInsets.fromLTRB(18, 0, 18, 18),
               child: Column(
@@ -81,7 +120,7 @@ class VolunteerDetailsPage extends StatelessWidget {
                             const SizedBox(width: 16),
 
                             // Name
-                            const Text("Blesson K Tomy", style: Styles.nameStyle),
+                            Text(volunteerDetails["name"]??"", style: Styles.nameStyle),
                           ],
                         ),
                       ],
@@ -91,78 +130,78 @@ class VolunteerDetailsPage extends StatelessWidget {
                   const SizedBox(height: 10),
 
                   // Age
-                  buildInfoContainer("Age:", value: volunteerDetails["age"]),
+                  buildInfoContainer("Age:", value:(volunteerDetails["age"]?.toString()??"0")),
 
                   const SizedBox(height: 10),
 
                   // Gender
-                  buildInfoContainer("Gender:", value: volunteerDetails["gender"]),
+                  buildInfoContainer("Gender:", value: (volunteerDetails["gender"]?.toString()??"")),
 
                   const SizedBox(height: 10),
 
                   // Rating
-                  buildInfoContainer("Rating:", value: volunteerDetails["rating"], isRating: true),
+                  buildInfoContainer("Rating:", value: (volunteerDetails["rating"]?.toString()??"0"), isRating: true),
 
                   const SizedBox(height: 10),
 
                   // Neighbourhood
-                  buildInfoContainer("Neighbourhood:", value: volunteerDetails["neighbourhood"]),
+                  buildInfoContainer("Neighbourhood:", value: neighbourhoodName),
 
                   const SizedBox(height: 10),
 
                   // Requests Completed
-                  buildInfoContainer("Requests Completed:", value: volunteerDetails["requestsCompleted"]),
+                  buildInfoContainer("Requests Completed:", value: (volunteerDetails["requestsCompleted"]?.toString()??"0")),
 
                   const SizedBox(height: 10),
 
                   // Services Provided
-                  buildInfoContainer("Services Provided:", services: volunteerDetails["servicesProvided"]),
+                  buildInfoContainer("Services Provided:", services: (volunteerDetails["services"] ??[])),
 
                   const SizedBox(height: 14),
 
                   // Cancel Request Button
-                  SizedBox(
-                    width: double.infinity,
-                    height: 56,
-                    child: TextButton(
-                      onPressed: () {
-                        showConfirmationDialog(context);
-                      },
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: Colors.red[500],
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(20),
-                          side: const BorderSide(color: Styles.offWhite, width: 2),
-                        ),
-                      ),
-                      child: const Row(
-                        mainAxisSize: MainAxisSize.min,
-                        children: [
-                          Icon(Icons.cancel, color: Colors.white, size: 26),
-                          SizedBox(width: 8),
-                          Text(
-                            "Remove Volunteer",
-                            style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 18,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
+                  // SizedBox(
+                  //   width: double.infinity,
+                  //   height: 56,
+                  //   child: TextButton(
+                  //     onPressed: () {
+                  //       showConfirmationDialog(context);
+                  //     },
+                  //     style: ElevatedButton.styleFrom(
+                  //       backgroundColor: Colors.red[500],
+                  //       shape: RoundedRectangleBorder(
+                  //         borderRadius: BorderRadius.circular(20),
+                  //         side: const BorderSide(color: Styles.offWhite, width: 2),
+                  //       ),
+                  //     ),
+                  //     child: const Row(
+                  //       mainAxisSize: MainAxisSize.min,
+                  //       children: [
+                  //         Icon(Icons.cancel, color: Colors.white, size: 26),
+                  //         SizedBox(width: 8),
+                  //         Text(
+                  //           "Remove Volunteer",
+                  //           style: TextStyle(
+                  //             color: Colors.white,
+                  //             fontSize: 18,
+                  //             fontWeight: FontWeight.bold,
+                  //           ),
+                  //         ),
+                  //       ],
+                  //     ),
+                  //   ),
+                  // ),
                 ],
               ),
             ),
           ],
         ),
       ),
-    );
+    );}
   }
 
   // Function to build info container dynamically
-  Widget buildInfoContainer(String title, {String value = '', bool isRating = false, List<String>? services}) {
+  Widget buildInfoContainer(String title, {String value = '', bool isRating = false, List<dynamic>? services}) {
     return Container(
       width: double.infinity,
       padding: const EdgeInsets.fromLTRB(20, 14, 20, 14),
@@ -171,50 +210,52 @@ class VolunteerDetailsPage extends StatelessWidget {
         borderRadius: BorderRadius.circular(20),
       ),
       child: services == null
-          ? Row(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-          "$title ",
-          style: Styles.bodyStyle,
-            ),
-            const SizedBox(height: 5),
-            if (isRating)
-          Row(
-            children: [
+            ? Wrap(
+              crossAxisAlignment: WrapCrossAlignment.start,
+              spacing: 10,
+              runSpacing: 5,
+              children: [
               Text(
-            value,
-            style: Styles.bodyStyle,
+                "$title ",
+                style: Styles.bodyStyle,
               ),
-              const SizedBox(width: 5),
-              Icon(
-            Icons.star,
-            color: Colors.yellow[500],
-            size: 20,
-              ),
-            ],
-          )
-            else
-          Text(
-            value,
-            style: Styles.bodyStyle,
-          ),
-          ],
-        )
+              if (isRating)
+                Row(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Text(
+                  value,
+                  style: Styles.bodyStyle,
+                  ),
+                  const SizedBox(width: 5),
+                  Icon(
+                  Icons.star,
+                  color: Colors.yellow[500],
+                  size: 20,
+                  ),
+                ],
+                )
+              else
+                Text(
+                value,
+                style: Styles.bodyStyle,
+                ),
+              ],
+            )
           : Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-          "$title ",
-          style: Styles.bodyStyle,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  "$title ",
+                  style: Styles.bodyStyle,
+                ),
+                const SizedBox(height: 5),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: services.map((service) => Text("• $service", style: Styles.bodyStyle)).toList(),
+                ),
+              ],
             ),
-            const SizedBox(height: 5),
-            Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: services.map((service) => Text("• $service", style: Styles.bodyStyle)).toList(),
-            ),
-          ],
-        ),
     );
   }
 
